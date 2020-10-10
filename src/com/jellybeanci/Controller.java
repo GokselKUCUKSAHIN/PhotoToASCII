df5055ec2bb91fa8e18261efed2b810b91a18b29
jellybeanci <gokselkucuksahin@gmail.com>
2020-10-11 00:46:37 +0300

    Naming.

--- src/com/jellybeanci/Controller.java
+++ src/com/jellybeanci/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package com.jellybeanci;
 
 public class Controller {
 }
--- src/com/jellybeanci/Main.java
+++ src/com/jellybeanci/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.jellybeanci;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,18 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("MainGUI.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
